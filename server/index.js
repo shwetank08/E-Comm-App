5945eded4aef8a810068f1c0cc1c58905caf2c18
shwetank08 <shwetanksingh.in@gmail.com>
2023-01-17 19:42:17 +0530

    configured cloudinary variables

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = require('./app');
 const cloudinary = require('cloudinary');
-const {PORT} = process.env;
+const {PORT, CLOUDINARY_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET} = process.env;
 
 cloudinary.config({ 
-    cloud_name: 'sample', 
-    api_key: '874837483274837', 
-    api_secret: 'a676b67565c6767a6767d6767f676fe1',
+    cloud_name: CLOUDINARY_NAME, 
+    api_key: CLOUDINARY_API_KEY, 
+    api_secret: CLOUDINARY_API_SECRET,
     secure: true
 });
 
