a567c18a87f7f1b592d1ad6916a8d9045f9062ff
shwetank08 <shwetanksingh.in@gmail.com>
2023-03-05 21:57:05 +0530

    fetching cart item bug solved

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -3,56 +3,67 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
 const Cart = () => {
-  const [items, setItems] = useState(null);
+  const [items, setItems] = useState([]);
+  const [productid, setProductId] = useState([]);
 
-  const handleUpdate = async() => {
+  const getAProduct = async (e) => {
+    const response = await fetch(`/api/${e}/getproduct`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+
+    const data = await response.json();
+    const itemlist = data.showOneProduct;
+    setItems((e) => [...e, itemlist]);
+  };
+  const handleUpdate = async () => {
     const arr = Object.keys(localStorage);
-    arr.forEach(async(e)=>{
-      const res = fetch(`/api/${e}/getproduct`,{
-        method: "GET",
-        headers:{
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        }
-      })
+    setProductId(arr);
+    productid.forEach(async (e) => {
+      getAProduct(e);
+    });
+  };
 
-      const data = await res.json();
-      console.log(data);
-    })
-    
-  }
-
-  useEffect(()=>{
+  useEffect(() => {
     handleUpdate();
-  },[])
+  }, [productid.length]);
 
   return (
     <Row>
-      <Col className="col-7">
-        {items &&
-          items.map((item, key) => {
-            return (
-              <Card style={{ width: "18rem" }} key={key}>
-                <Card.Img variant="top" src={item.photos?.secure_url} />
-                <Card.Body className="flex justify-center flex-col">
-                  <Card.Title className="text-center flex justify-around">
-                    <Card.Text>{item.name}</Card.Text>
-                    <Card.Text>&#8377;{item.price}</Card.Text>
-                  </Card.Title>
-                  <Card.Text className="text-center">
-                    {item.description}
-                  </Card.Text>
-                  <Button variant="primary">
-                    Remove
-                  </Button>
-                </Card.Body>
-              </Card>
-            );
-          })}
-      </Col>
-      <Col className="col-5"></Col>
+      <Container
+        style={{
+          marginTop: "5rem",
+          marginBottom: "5rem",
+        }}
+      >
+        <Col className="col-8 d-flex flex-column flex-wrap">
+          {items &&
+            items.map((item) => {
+              return (
+                <Card style={{ width: "14rem" }} key={item._id}>
+                  <Card.Img variant="top" src={item.photos?.secure_url} />
+                  <Card.Body className="flex justify-center flex-col">
+                    <Card.Title className="text-center flex justify-around">
+                      <Card.Text>{item.name}</Card.Text>
+                      <Card.Text>&#8377;{item.price}</Card.Text>
+                    </Card.Title>
+                    <Card.Text className="text-center">
+                      {item.description}
+                    </Card.Text>
+                    <Button variant="primary">Remove</Button>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+        </Col>
+      </Container>
+      <Col className="col-4"></Col>
     </Row>
   );
 };
