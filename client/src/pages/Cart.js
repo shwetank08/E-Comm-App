431babcedbe3b78d04452e0eefbfcce332ce101d
shwetank08 <shwetanksingh.in@gmail.com>
2023-03-06 21:23:29 +0530

    working on cart

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -8,6 +8,7 @@ import Container from "react-bootstrap/Container";
 const Cart = () => {
   const [items, setItems] = useState([]);
   const [productid, setProductId] = useState([]);
+  const [amount, setAmount] = useState(0);
 
   const getAProduct = async (e) => {
     const response = await fetch(`/api/${e}/getproduct`, {
@@ -30,41 +31,66 @@ const Cart = () => {
     });
   };
 
+  const handleRemove = async(id) => {
+    localStorage.removeItem(id);
+    setItems((item)=>{
+      item.filter((e)=>e._id!=id);
+    })
+  }
+
+  const handleAmount = () => { 
+    const sum = items.reduce((cur, acc)=>{
+      return cur+acc;
+    },0)
+    console.log(sum);
+    setAmount(sum);
+  }
+
+  const DisplayItems = () => {
+      return (
+        <>
+        {items &&
+          items.map((item) => {
+            return (
+              <Card style={{ width: "16rem"}} key={item._id}>
+                <Card.Img variant="top" src={item.photos?.secure_url}/>
+                <Card.Body className="flex justify-center flex-col">
+                  <Card.Title className="text-center flex justify-around">
+                    <Card.Text>{item.name}</Card.Text>
+                    <Card.Text>&#8377;{item.price}</Card.Text>
+                  </Card.Title>
+                  <Card.Text className="text-center">
+                    {item.description}
+                  </Card.Text>
+                  <Button variant="primary" onClick={()=>{handleRemove(item._id)}}>Remove</Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+          </>
+      )
+  }
+
   useEffect(() => {
     handleUpdate();
+    handleAmount();
   }, [productid.length]);
 
   return (
+    <Container style={{marginTop: "5rem"}}>
     <Row>
-      <Container
-        style={{
-          marginTop: "5rem",
-          marginBottom: "5rem",
-        }}
-      >
-        <Col className="col-8 d-flex flex-column flex-wrap">
-          {items &&
-            items.map((item) => {
-              return (
-                <Card style={{ width: "14rem" }} key={item._id}>
-                  <Card.Img variant="top" src={item.photos?.secure_url} />
-                  <Card.Body className="flex justify-center flex-col">
-                    <Card.Title className="text-center flex justify-around">
-                      <Card.Text>{item.name}</Card.Text>
-                      <Card.Text>&#8377;{item.price}</Card.Text>
-                    </Card.Title>
-                    <Card.Text className="text-center">
-                      {item.description}
-                    </Card.Text>
-                    <Button variant="primary">Remove</Button>
-                  </Card.Body>
-                </Card>
-              );
-            })}
+        <Col className="col-4 d-flex flex-column flex-wrap gap-1">
+          <DisplayItems/>
         </Col>
-      </Container>
-      <Col className="col-4"></Col>
+      <Col className="col-8">
+      <Container>
+        <h1>CART CONTENT</h1>
+        <h2>Amount - {amount}</h2>
+      </Container>  
+      </Col>
     </Row>
+    </Container>
+
   );
 };
 
