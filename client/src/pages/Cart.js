53b90b3f67938516ef9695dca19f107b0e260259
shwetank08 <shwetanksingh.in@gmail.com>
2023-05-06 23:19:49 +0530

    working on order route

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -24,6 +24,13 @@ const Cart = () => {
     state: "",
     contactNumber: "",
     postalCode: "",
+  });
+  const [orderItems, setOrderItems] = useState({
+    name: "",
+    quantity: "",
+    image: "",
+    price: "",
+    product: ""
   })
   const navigate = useNavigate();
   const context = useContext(userContext);
@@ -128,32 +135,47 @@ const Cart = () => {
       console.log(err);
     }
   };
+  let form = new FormData();
+  form.append("email", shippingAddress.email);
+  form.append("address", shippingAddress.address);
+  form.append("city", shippingAddress.city);
+  form.append("state", shippingAddress.state);
+  form.append("contactNumber", shippingAddress.contactNumber);
+  form.append("postalCode", shippingAddress.postalCode);
+  form.append("orderItems", shippingAddress.orderItems);
+  form.append("totalAmount", shippingAddress.totalAmount);
 
-  const handleShipping = async() => {
-    try{
-    const response = fetch(`/api/${id}/order/create`,{
-      method: "POST",
-      body: JSON.stringify({
-        email: shippingAddress.email,
-        address: shippingAddress.address,
-        city: shippingAddress.city,
-        state: shippingAddress.state,
-        contactNumber: shippingAddress.contactNumber,
-        postalCode: shippingAddress.postalCode,
-        orderItems: [{
-          name: items.name,
-          quantity: items.stock,
-          image: items.photos?.secure_url,
-          price: items.price,
-          product: items._id
-      }],
-        totalAmount: price
-      }),
-    });
-  }catch(err){
-    console.log(err);
-  }
-  }
+  const handleShipping = async () => {
+    try {
+      // setOrderItems({
+        
+      // })
+      console.log(shippingAddress);
+      const response = fetch(`/api/${id}/order/create`, {
+        method: "POST",
+        body: JSON.stringify({
+          email: shippingAddress.email,
+          address: shippingAddress.address,
+          city: shippingAddress.city,
+          state: shippingAddress.state,
+          contactNumber: shippingAddress.contactNumber,
+          postalCode: shippingAddress.postalCode,
+          // orderItems: [
+          //   {
+          //     name: items.name,
+          //     quantity: items.stock,
+          //     image: items.photos?.secure_url,
+          //     price: items.price,
+          //     product: items._id,
+          //   },
+          // ],
+          totalAmount: price,
+        }),
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const DisplayItems = () => {
     return (
@@ -226,57 +248,112 @@ const Cart = () => {
                 <h2>&#8377;{amount}</h2>
               </h2>
 
-              <Form style={{backgroundColor: "#D3D3D4", padding: "1.5rem"}}>
+              <Form style={{ backgroundColor: "#D3D3D4", padding: "1.5rem" }}>
                 <h1 className="d-flex justify-center py-2">Shipping Address</h1>
                 <Row className="mb-3">
                   <Form.Group as={Col} controlId="formGridEmail">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email"
-                    onChange={(e) => {
-                      setShippingAddress({ ...shippingAddress, email: e.target.value });
-                    }}/>
+                    <Form.Control
+                      type="email"
+                      placeholder="Enter email"
+                      value={shippingAddress.email ? shippingAddress.email : ""}
+                      onChange={(e) => {
+                        setShippingAddress({
+                          ...shippingAddress,
+                          email: e.target.value,
+                        });
+                      }}
+                    />
                   </Form.Group>
 
                   <Form.Group as={Col} controlId="formGridPassword">
                     <Form.Label>Contact Number</Form.Label>
-                    <Form.Control type="number" placeholder="1234567890" onChange={(e) => {
-                      setShippingAddress({ ...shippingAddress, contactNumber: e.target.value });
-                    }}/>
+                    <Form.Control
+                      type="number"
+                      placeholder="1234567890"
+                      value={
+                        shippingAddress.contactNumber
+                          ? shippingAddress.contactNumber
+                          : ""
+                      }
+                      onChange={(e) => {
+                        setShippingAddress({
+                          ...shippingAddress,
+                          contactNumber: e.target.value,
+                        });
+                      }}
+                    />
                   </Form.Group>
                 </Row>
 
                 <Form.Group className="mb-3" controlId="formGridAddress1">
                   <Form.Label>Address</Form.Label>
-                  <Form.Control as="textarea" rows={3} placeholder="1234 Main St" onChange={(e) => {
-                    setShippingAddress({ ...shippingAddress, address: e.target.value });
-                  }}/>
+                  <Form.Control
+                    as="textarea"
+                    rows={3}
+                    value={
+                      shippingAddress.address ? shippingAddress.address : ""
+                    }
+                    placeholder="1234 Main St"
+                    onChange={(e) => {
+                      setShippingAddress({
+                        ...shippingAddress,
+                        address: e.target.value,
+                      });
+                    }}
+                  />
                 </Form.Group>
-
 
                 <Row className="mb-3">
                   <Form.Group as={Col} controlId="formGridCity">
                     <Form.Label>City</Form.Label>
-                    <Form.Control onChange={(e) => {
-                      setShippingAddress({ ...shippingAddress, city: e.target.value });
-                    }}/>
+                    <Form.Control
+                      onChange={(e) => {
+                        setShippingAddress({
+                          ...shippingAddress,
+                          city: e.target.value,
+                        });
+                      }}
+                    />
                   </Form.Group>
 
                   <Form.Group as={Col} controlId="formGridCity">
                     <Form.Label>State</Form.Label>
-                    <Form.Control onChange={(e) => {
-                      setShippingAddress({ ...shippingAddress, state: e.target.value });
-                    }}/>
+                    <Form.Control
+                      value={shippingAddress.state ? shippingAddress.state : ""}
+                      onChange={(e) => {
+                        setShippingAddress({
+                          ...shippingAddress,
+                          state: e.target.value,
+                        });
+                      }}
+                    />
                   </Form.Group>
 
                   <Form.Group as={Col} controlId="formGridZip">
                     <Form.Label>Zip</Form.Label>
-                    <Form.Control type="number" onChange={(e) => {
-                      setShippingAddress({ ...shippingAddress, postalCode: e.target.value });
-                    }}/>
+                    <Form.Control
+                      type="number"
+                      value={
+                        shippingAddress.postalCode
+                          ? shippingAddress.postalCode
+                          : ""
+                      }
+                      onChange={(e) => {
+                        setShippingAddress({
+                          ...shippingAddress,
+                          postalCode: e.target.value,
+                        });
+                      }}
+                    />
                   </Form.Group>
                 </Row>
                 <StripeCheckout token={makePaymentToken} stripeKey={sKey}>
-                  <Button variant="primary" className="mt-2 w-100" onClick={handleShipping}>
+                  <Button
+                    variant="primary"
+                    className="mt-2 w-100"
+                    onClick={handleShipping}
+                  >
                     Check Out
                   </Button>
                 </StripeCheckout>
